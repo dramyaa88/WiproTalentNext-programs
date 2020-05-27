c9f3844199f86e028a995625d4d73363ffbfc29c
dramyaa88 <58719475+dramyaa88@users.noreply.github.com>
2020-05-27 17:47:42 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,13 @@
+Write a function to find whether the given input number is odd
+if the number is odd return 2 else return 1
+
+class Odd
+{
+public int number(int input1)
+{
+if(input1%2==0)
+  return 1;
+else
+  return 1;
+}
+}
