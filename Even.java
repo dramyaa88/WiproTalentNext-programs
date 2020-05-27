ad3bcb448075289619d979b72b1df649b0b86d62
dramyaa88 <58719475+dramyaa88@users.noreply.github.com>
2020-05-27 17:42:25 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,13 @@
+Write a function to find whether the given input is Even.
+if the given number is even the function should return 2 else it should return 1
+
+Class Even
+{
+public int number(int input1)
+{
+if(input1%2==0)
+  return 2;
+else
+  return 1;
+}
+}
