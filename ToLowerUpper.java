1b0f1fa46ac4903870bd6b9744d7203d770773da
dramyaa88 <58719475+dramyaa88@users.noreply.github.com>
2020-05-29 17:50:21 +0530

    Create ToLowerUpper.java

---
+++ ToLowerUpper.java
@@ -0,0 +1,17 @@
+Write a program to convert from upper case to lower case and vice versa of an alphabet and printthe old character and new 
+character as shown in example
+
+(Ex: a->A, M->m).
+
+public class iflowerupper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a='L';
+		char b='d';
+		System.out.println(a+"->"+Character.toLowerCase(a));
+		System.out.println(b+"->"+Character.toUpperCase(b));
+
+	}
+
+}
