d9a3c035f53af4d444be9c7f889c09494a0d0cc5
dramyaa88 <58719475+dramyaa88@users.noreply.github.com>
2020-06-07 21:20:52 +0530

    Create 2ndStringToUppercase.java

---
+++ 2ndStringToUppercase.java
@@ -0,0 +1,11 @@
+public int uppercase(String input1)
+{
+String s[] = input1.split(" ");
+		if(s.length==1)
+		{
+			return "LESS";
+		}
+		String s1 = s[1];
+		s1 = s1.toUpperCase();
+		return s1;
+}
